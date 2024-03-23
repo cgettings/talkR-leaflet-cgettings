e0615480b691653e045327907808f61ecedb7a3a
cgettings <gettings.chris@gmail.com>
2024-03-22 23:30:56 -0400

    install package script also included in every Rmd

---
+++ code/_0_install_packages.R
@@ -0,0 +1,22 @@
+
+# These are the packages you already have
+
+packages_you_have <- installed.packages()[, "Package"]
+
+# These are all the packages we'll use
+
+packages_we_use <- c("jsonlite", "dplyr", "tibble", "tidyr", "leaflet", "leaflet.extras", "leafem", "terra", "stars", "geojsonio", "sf", "viridisLite", "here", "htmlwidgets", "htmltools", "conflicted")
+
+# These are the packages you need to install
+
+packages_you_need <- packages_we_use[!packages_we_use %in% packages_you_have]
+
+# This will install the packages you don't already have
+
+if (length(packages_you_need)) {
+    
+    cat("Installing:", paste(packages_you_need, collapse = ", "))
+    
+    install.packages(packages_you_need)
+}
+
